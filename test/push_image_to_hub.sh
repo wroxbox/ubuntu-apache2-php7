3c0424b789d3304b5e748958babe9f429ae42e21
Antti Tuppurainen <antti.tuppurainen@industry62.com>
2017-10-30 22:24:20 +0200

    Add php 7 tools for machine

--- test/push_image_to_hub.sh
+++ test/push_image_to_hub.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/mobingi/ubuntu-apache2-php7/trigger/${DOCKER_BUILD_TOKEN}/
+curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/wroxbox/ubuntu-apache2-php7/trigger/${DOCKER_BUILD_TOKEN}/
